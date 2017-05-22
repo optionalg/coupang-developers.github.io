b7e6446ac99ab850a1e59ba7f2bbbe3cc5bb93c9
DarkKnight <polyglot.m@coupang.com>
2017-05-22 15:20:42 +0900

    release(1.0.5)

--- assets/docs/season-1/delivery-api/ko/products-already-shipped-processing.document.ts
+++ assets/docs/season-1/delivery-api/ko/products-already-shipped-processing.document.ts
@@ -1,6 +1,6 @@
 export const productAlreadyShippedProcessingDocument = {
   note: ``,
-
+  
   //don't modify documentInfo
   documentInfo: {
     category: `stop-shipping-api`,   // input category ex) exchange-service-api
@@ -15,9 +15,9 @@ export const productAlreadyShippedProcessingDocument = {
       name: ``,
       anchorId: ``,
     },
-
+    
   },
-
+  
   apiInfo: {
     state: `draft`,      // draft, candidate, release, unstable, stable, deprecated
     lastUpdateDate: `2017-02-17`, // yyyy-mm-dd  ex> 2016-12-23
@@ -63,15 +63,206 @@ export const productAlreadyShippedProcessingDocument = {
         _warning: ``,
         children: false
       }, {
-        name: `cancelCount`,
-        type: `Number`,
+        name: `deliveryCompanyCode`,
+        type: `String`,
         require: false,
-        _description: `출고 수량`,
+        _description: ``,
+        _relation: ``,
+        _referenceInfo: `<table>
+  <tr>
+  <th>code</th>
+  <th>name</th>
+  </tr>
+  <tr>
+    <td>HYUNDAI</td>
+    <td>롯데택배</td>
+  </tr>
+  <tr>
+    <td>KGB</td>
+    <td>로젠택배</td>
+  </tr>
+  <tr>
+    <td>EPOST</td>
+    <td>우체국</td>
+  </tr>
+  <tr>
+    <td>HANJIN</td>
+    <td>한진택배</td>
+  </tr>
+  <tr>
+    <td>KOREX</td>
+    <td>대한통운</td>
+  </tr>
+  <tr>
+    <td>CJGLS</td>
+    <td>CJ대한통운</td>
+  </tr>
+  <tr>
+    <td><span style="text-decoration: line-through">LOGEN</span> -> KGB</td>
+    <td>로젠택배</td>
+  </tr>
+  <tr>
+    <td>KGBLS</td>
+    <td>KGB택배</td>
+  </tr>
+  <tr>
+    <td>KDEXP</td>
+    <td>경동택배</td>
+  </tr>
+  <tr>
+    <td>DONGBU</td>
+    <td>KG로지스</td>
+  </tr>
+  <tr>
+    <td>INNOGIS</td>
+    <td>GTX로지스</td>
+  </tr>
+  <tr>
+    <td><span style="text-decoration: line-through">KYUNGDONG</span> -> KDEXP</td>
+    <td>경동택배</td>
+  </tr>
+  <tr>
+    <td>ILYANG</td>
+    <td>일양택배</td>
+  </tr>
+  <tr>
+    <td>CHUNIL</td>
+    <td>천일택배</td>
+  </tr>
+  <tr>
+    <td>AJOU</td>
+    <td>아주택배</td>
+  </tr>
+  <tr>
+    <td>CSLOGIS</td>
+    <td>SC로지스</td>
+  </tr>
+  <tr>
+    <td>DAESIN</td>
+    <td>대신택배</td>
+  </tr>
+  <tr>
+    <td>CVS</td>
+    <td>CVS택배</td>
+  </tr>
+  <tr>
+    <td>HDEXP</td>
+    <td>합동택배</td>
+  </tr>
+  <tr>
+    <td>DADREAM</td>
+    <td>다드림</td>
+  </tr>
+  <tr>
+    <td>DHL</td>
+    <td>DHL</td>
+  </tr>
+  <tr>
+    <td>UPS</td>
+    <td>UPS</td>
+  </tr>
+  <tr>
+    <td>FEDEX</td>
+    <td>FEDEX</td>
+  </tr>
+  <tr>
+    <td>REGISTPOST</td>
+    <td>우편등기</td>
+  </tr>
+  <tr>
+    <td>DIRECT</td>
+    <td>업체직송</td>
+  </tr>
+  <tr>
+    <td>COUPANG</td>
+    <td>쿠팡자체배송</td>
+  </tr>
+  <tr>
+    <td>IQS</td>
+    <td>굿스포스트</td>
+  </tr>
+  <tr>
+    <td>EMS</td>
+    <td>우체국 EMS</td>
+  </tr>
+  <tr>
+    <td>TNT</td>
+    <td>TNT</td>
+  </tr>
+  <tr>
+    <td>USPS</td>
+    <td>USPS</td>
+  </tr>
+  <tr>
+    <td>IPARCEL</td>
+    <td>i-parcel</td>
+  </tr>
+  <tr>
+    <td>GSMNTON</td>
+    <td>GSM NtoN</td>
+  </tr>
+  <tr>
+    <td>SWGEXP</td>
+    <td>성원글로벌</td>
+  </tr>
+  <tr>
+    <td>PANTOS</td>
+    <td>범한판토스</td>
+  </tr>
+  <tr>
+    <td>ACIEXPRESS</td>
+    <td>ACI Express</td>
+  </tr>
+  <tr>
+    <td>DAEWOON</td>
+    <td>대운글로벌</td>
+  </tr>
+  <tr>
+    <td>AIRBOY</td>
+    <td>에어보이익스프레스</td>
+  </tr>
+  <tr>
+    <td>KGLNET</td>
+    <td>KGL네트웍스</td>
+  </tr>
+  <tr>
+    <td>KUNYOUNG</td>
+    <td>건영택배</td>
+  </tr>
+  <tr>
+    <td>SLX</td>
+    <td>SLX택배</td>
+  </tr>
+  <tr>
+    <td>HONAM</td>
+    <td>호남택배</td>
+  </tr>
+  <tr>
+    <td>LINEEXPRESS</td>
+    <td>LineExpress</td>
+  </tr>
+  <tr>
+    <td>SFEXPRESS</td>
+    <td>순풍택배</td>
+  </tr>
+  <tr>
+    <td>TWOFASTEXP</td>
+    <td>2FastsExpress</td>
+  </tr>
+</table>
+`,
+        _warning: ``,
+        children: false
+      }, {
+        name: `invoiceNumber`,
+        type: `String`,
+        require: false,
+        _description: `송장번호`,
         _relation: ``,
         _referenceInfo: ``,
         _warning: ``,
         children: false
-      },
+      }
     ]
   },
   errorSpec: false,
@@ -122,23 +313,24 @@ export const productAlreadyShippedProcessingDocument = {
     }
   ],
   sample: {
-    endpoint: `https://api-gateway.coupang.com/v2/providers/wing_api/apis/internal-api/v1/return/363667/release-stop`,
+    endpoint: `https://api-gateway.coupang.com/v2/providers/wing_api/apis/internal-api/v1/return/12346897/release-stop`,
     code: [
       {
         language: `http`,
         codeblock: {
-          "vendorId": "A00013264",
-          "receiptId": 363667,
-          "cancelCount": 2
+          "vendorId": "TESTVENDORID",
+          "receiptId": 12346897,
+          "deliveryCompanyCode": "KDEXP",
+          "invoiceNumber": "70432743577"
         }
       }
     ],
     response: {
-      "code": "200",
-      "message": "OK",
       "data": {
+        "code": "200",
+        "message": "OK",
         "resultCode": "SUCCESS",
-        "resultMessage": "출고중지완료요청이 성공하였습니다"
+        "resultMessage": "이미출고 상태변경이 성공하였습니다."
       }
     },
     _description: ``,
--- assets/docs/season-1/delivery-api/ko/stop-shipping-complete-processing.document.ts
+++ assets/docs/season-1/delivery-api/ko/stop-shipping-complete-processing.document.ts
@@ -1,6 +1,6 @@
 export const stopShippingCompleteProcessingDocument = {
   note: ``,
-
+  
   //don't modify documentInfo
   documentInfo: {
     category: `stop-shipping-api`,   // input category ex) exchange-service-api
@@ -15,9 +15,9 @@ export const stopShippingCompleteProcessingDocument = {
       name: ``,
       anchorId: ``,
     },
-
+    
   },
-
+  
   apiInfo: {
     state: ``,      // draft, candidate, release, unstable, stable, deprecated
     lastUpdateDate: ``, // yyyy-mm-dd  ex> 2016-12-23
@@ -63,206 +63,15 @@ export const stopShippingCompleteProcessingDocument = {
         _warning: ``,
         children: false
       }, {
-        name: `deliveryCompanyCode`,
-        type: `String`,
+        name: `cancelCount`,
+        type: `Number`,
         require: false,
-        _description: ``,
-        _relation: ``,
-        _referenceInfo: `<table>
-  <tr>
-  <th>code</th>
-  <th>name</th>
-  </tr>
-  <tr>
-    <td>HYUNDAI</td>
-    <td>롯데택배</td>
-  </tr>
-  <tr>
-    <td>KGB</td>
-    <td>로젠택배</td>
-  </tr>
-  <tr>
-    <td>EPOST</td>
-    <td>우체국</td>
-  </tr>
-  <tr>
-    <td>HANJIN</td>
-    <td>한진택배</td>
-  </tr>
-  <tr>
-    <td>KOREX</td>
-    <td>대한통운</td>
-  </tr>
-  <tr>
-    <td>CJGLS</td>
-    <td>CJ대한통운</td>
-  </tr>
-  <tr>
-    <td><span style="text-decoration: line-through">LOGEN</span> -> KGB</td>
-    <td>로젠택배</td>
-  </tr>
-  <tr>
-    <td>KGBLS</td>
-    <td>KGB택배</td>
-  </tr>
-  <tr>
-    <td>KDEXP</td>
-    <td>경동택배</td>
-  </tr>
-  <tr>
-    <td>DONGBU</td>
-    <td>KG로지스</td>
-  </tr>
-  <tr>
-    <td>INNOGIS</td>
-    <td>GTX로지스</td>
-  </tr>
-  <tr>
-    <td><span style="text-decoration: line-through">KYUNGDONG</span> -> KDEXP</td>
-    <td>경동택배</td>
-  </tr>
-  <tr>
-    <td>ILYANG</td>
-    <td>일양택배</td>
-  </tr>
-  <tr>
-    <td>CHUNIL</td>
-    <td>천일택배</td>
-  </tr>
-  <tr>
-    <td>AJOU</td>
-    <td>아주택배</td>
-  </tr>
-  <tr>
-    <td>CSLOGIS</td>
-    <td>SC로지스</td>
-  </tr>
-  <tr>
-    <td>DAESIN</td>
-    <td>대신택배</td>
-  </tr>
-  <tr>
-    <td>CVS</td>
-    <td>CVS택배</td>
-  </tr>
-  <tr>
-    <td>HDEXP</td>
-    <td>합동택배</td>
-  </tr>
-  <tr>
-    <td>DADREAM</td>
-    <td>다드림</td>
-  </tr>
-  <tr>
-    <td>DHL</td>
-    <td>DHL</td>
-  </tr>
-  <tr>
-    <td>UPS</td>
-    <td>UPS</td>
-  </tr>
-  <tr>
-    <td>FEDEX</td>
-    <td>FEDEX</td>
-  </tr>
-  <tr>
-    <td>REGISTPOST</td>
-    <td>우편등기</td>
-  </tr>
-  <tr>
-    <td>DIRECT</td>
-    <td>업체직송</td>
-  </tr>
-  <tr>
-    <td>COUPANG</td>
-    <td>쿠팡자체배송</td>
-  </tr>
-  <tr>
-    <td>IQS</td>
-    <td>굿스포스트</td>
-  </tr>
-  <tr>
-    <td>EMS</td>
-    <td>우체국 EMS</td>
-  </tr>
-  <tr>
-    <td>TNT</td>
-    <td>TNT</td>
-  </tr>
-  <tr>
-    <td>USPS</td>
-    <td>USPS</td>
-  </tr>
-  <tr>
-    <td>IPARCEL</td>
-    <td>i-parcel</td>
-  </tr>
-  <tr>
-    <td>GSMNTON</td>
-    <td>GSM NtoN</td>
-  </tr>
-  <tr>
-    <td>SWGEXP</td>
-    <td>성원글로벌</td>
-  </tr>
-  <tr>
-    <td>PANTOS</td>
-    <td>범한판토스</td>
-  </tr>
-  <tr>
-    <td>ACIEXPRESS</td>
-    <td>ACI Express</td>
-  </tr>
-  <tr>
-    <td>DAEWOON</td>
-    <td>대운글로벌</td>
-  </tr>
-  <tr>
-    <td>AIRBOY</td>
-    <td>에어보이익스프레스</td>
-  </tr>
-  <tr>
-    <td>KGLNET</td>
-    <td>KGL네트웍스</td>
-  </tr>
-  <tr>
-    <td>KUNYOUNG</td>
-    <td>건영택배</td>
-  </tr>
-  <tr>
-    <td>SLX</td>
-    <td>SLX택배</td>
-  </tr>
-  <tr>
-    <td>HONAM</td>
-    <td>호남택배</td>
-  </tr>
-  <tr>
-    <td>LINEEXPRESS</td>
-    <td>LineExpress</td>
-  </tr>
-  <tr>
-    <td>SFEXPRESS</td>
-    <td>순풍택배</td>
-  </tr>
-  <tr>
-    <td>TWOFASTEXP</td>
-    <td>2FastsExpress</td>
-  </tr>
-</table>
-`,
-        _warning: ``,
-        children: false
-      }, {
-        name: `invoiceNumber`,
-        type: `String`,
-        require: false,
-        _description: `송장번호`,
+        _description: `출고 수량`,
         _relation: ``,
         _referenceInfo: ``,
         _warning: ``,
         children: false
-      }
+      },
     ]
   },
   errorSpec: false,
@@ -313,24 +122,23 @@ export const stopShippingCompleteProcessingDocument = {
     }
   ],
   sample: {
-    endpoint: `https://api-gateway.coupang.com/v2/providers/wing_api/apis/internal-api/v1/return/12346897/release-stop`,
+    endpoint: `https://api-gateway.coupang.com/v2/providers/wing_api/apis/internal-api/v1/return/363667/release-stop`,
     code: [
       {
         language: `http`,
         codeblock: {
-          "vendorId": "TESTVENDORID",
-          "receiptId": 12346897,
-          "deliveryCompanyCode": "KDEXP",
-          "invoiceNumber": "70432743577"
+          "vendorId": "A00013264",
+          "receiptId": 363667,
+          "cancelCount": 2
         }
       }
     ],
     response: {
+      "code": "200",
+      "message": "OK",
       "data": {
-        "code": "200",
-        "message": "OK",
         "resultCode": "SUCCESS",
-        "resultMessage": "이미출고 상태변경이 성공하였습니다."
+        "resultMessage": "출고중지완료요청이 성공하였습니다"
       }
     },
     _description: ``,
@@ -338,4 +146,5 @@ export const stopShippingCompleteProcessingDocument = {
     _referenceInfo: ``,
     _warning: ``
   }
+  
 }
